479e20f6b93c9dc083a1effbe741006dd07b9959
Edilson-ti <info.edilson@gmail.com>
2026-05-03 23:12:05 -0300

    Update Monitoramento-Exclusao.ps1

--- scripts/Monitoramento-Exclusao.ps1
+++ scripts/Monitoramento-Exclusao.ps1
@@ -9,3 +9,12 @@ Register-WmiEvent -Query "SELECT * FROM __InstanceDeletionEvent WITHIN 5 WHERE T
         Write-Output "ALERTA: Possível exclusão em massa detectada!"
     }
 }
+# Enviar alerta por e-mail
+
+$smtpServer = "smtp.mail.***.com"
+$emailFrom = "edilson.pereira@****.com"
+$emailTo = "admin@email.com"
+$subject = "ALERTA DE SEGURANÇA"
+$body = "Exclusão em massa detectada!"
+
+Send-MailMessage -SmtpServer $smtpServer -From $emailFrom -To $emailTo -Subject $subject -Body $body
