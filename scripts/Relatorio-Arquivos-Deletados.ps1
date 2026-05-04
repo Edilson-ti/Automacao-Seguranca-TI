17f2d840a9bf6a5be1bdd6f274656fe0f6ec3959
Edilson Pereira <info.edilson@gmail.com>
2026-05-03 23:26:09 -0300

    Update Relatorio-Arquivos-Deletados.ps1

--- scripts/Relatorio-Arquivos-Deletados.ps1
+++ scripts/Relatorio-Arquivos-Deletados.ps1
@@ -1,7 +1,29 @@
+<#
+.Info
+Gera relatório de arquivos deletados com base nos logs de segurança do Windows.
+
+.Função
+O script consulta o Event Viewer (log de segurança) buscando eventos relacionados
+à manipulação de arquivos (ID 4663), permitindo auditoria e rastreabilidade.
+
+.Autor
+Edilson Gomes Pereira
+#>
+
+Write-Output "Coletando eventos de auditoria..."
+
+# Coleta eventos do log de segurança
 $eventos = Get-WinEvent -LogName Security | Where-Object {
     $_.Id -eq 4663
 }
 
+# Seleciona informações relevantes
 $relatorio = $eventos | Select-Object TimeCreated, Message
 
-$relatorio | Export-Csv "C:\Relatorio_Arquivos_Deletados.csv" -NoTypeInformation
+# Caminho do arquivo de saída
+$caminhoSaida = "\\Fileserver\auditoria$\Scripts\Resultados\Arquivos_Deletados.csv"
+
+# Exporta relatório
+$relatorio | Export-Csv $caminhoSaida -NoTypeInformation
+
+Write-Output "Relatório gerado em: $caminhoSaida"
